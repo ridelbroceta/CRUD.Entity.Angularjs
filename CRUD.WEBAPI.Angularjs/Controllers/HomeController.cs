18a5caf3af28a434502e6e0c16e2886484a731f1
ridel <ridel.broceta@gmail.com>
2016-11-24 21:17:36 -0500

    11-24-16

--- CRUD.WEBAPI.Angularjs/Controllers/HomeController.cs
+++ CRUD.WEBAPI.Angularjs/Controllers/HomeController.cs
@@ -14,5 +14,6 @@ namespace CRUD.WEBAPI.Angularjs.Controllers
 
             return View();
         }
+
     }
 }
---
+++ CRUD.WEBAPI.Angularjs/Scripts/MyScripts/directive.js
@@ -0,0 +1,16 @@
+﻿app.directive('testDir', function () {
+    return {
+        restrict: 'E',
+        templateUrl: '/Content/Templates/directive.html',
+        scope: {
+            action: "@",
+            hideid: "=",
+            id: "@",
+            name: "=", //variables de alcance($scope) o por valor
+            lastname: "=", //usado para hacer uso de data-binding(datos entre vista controlador) o por referencia
+            state: "=",
+            age: "=",
+            clickaction: "&" //útiles para llamar a funciones
+        }
+    }
+})
